50d5cfb3c0c0a29f91c6b2bd4cd85380b2cee818
Hala Emad <35690579+HalaEH@users.noreply.github.com>
2022-06-01 19:04:24 +0200

    payment_discountCodes

---
+++ app/src/main/java/com/gradle/shopifyapp/payment/PaymentActivity.kt
@@ -0,0 +1,12 @@
+package com.gradle.shopifyapp.payment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gradle.shopifyapp.R
+
+class PaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+}
