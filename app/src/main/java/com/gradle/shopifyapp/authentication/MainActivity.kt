a80afd5a93ed725f35a5786abeed7c3c0a49041d
Rana-Abdelaziz <61909090+Rana-Abdelaziz@users.noreply.github.com>
2022-06-06 15:49:58 +0200

    register/user/api

--- app/src/main/java/com/gradle/shopifyapp/authentication/MainActivity.kt
+++ app/src/main/java/com/gradle/shopifyapp/authentication/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.gradle.shopifyapp.authentication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.SyncStateContract
+import com.gradle.shopifyapp.MainTabsActivity
 import com.gradle.shopifyapp.R
+import com.kotlin.weatherforecast.utils.Constants
+import com.kotlin.weatherforecast.utils.MyPreference
 
 class MainActivity : AppCompatActivity() {
+    lateinit var sharedPreference: MyPreference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sharedPreference = MyPreference.getInstance(this)!!
+        val check = sharedPreference.getData(Constants.USERID)
+        if(!check.isNullOrEmpty()){
+            startActivity(Intent(this,MainTabsActivity::class.java))
+        }
+
     }
 }
--- app/src/main/java/com/gradle/shopifyapp/network/ApiClient.kt
+++ app/src/main/java/com/gradle/shopifyapp/network/ApiClient.kt
@@ -19,7 +19,8 @@ class ApiClient private constructor() : RemoteSource {
     }
 
     object RetrofitHelper {
-        val baseUrl = "https://madalex20220.myshopify.com/admin/api/"
+        //val baseUrl = "https://madalex20220.myshopify.com/admin/api/"
+        val baseUrl ="https://9d169ad72dd7620e70f56b28ae6146d9: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@madalex20220.myshopify.com/admin/api/"
 
 
         fun getInstance(): Retrofit {
