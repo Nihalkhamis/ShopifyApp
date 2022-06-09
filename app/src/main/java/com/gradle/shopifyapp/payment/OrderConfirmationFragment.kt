2e900f5032b84108d2835fbf2f429caad174195e
Hala Emad <35690579+HalaEH@users.noreply.github.com>
2022-06-09 11:22:17 +0200

    order_confirmation

--- app/src/main/java/com/gradle/shopifyapp/payment/OrderConfirmationFragment.kt
+++ app/src/main/java/com/gradle/shopifyapp/payment/OrderConfirmationFragment.kt
@@ -10,11 +10,14 @@ import androidx.navigation.Navigation
 import com.gradle.shopifyapp.R
 import com.gradle.shopifyapp.databinding.FragmentOrderConfirmationBinding
 import com.gradle.shopifyapp.databinding.FragmentStartingBinding
+import com.kotlin.weatherforecast.utils.Constants
+import com.kotlin.weatherforecast.utils.MyPreference
 
 
 class OrderConfirmationFragment : Fragment() {
     private var _binding: FragmentOrderConfirmationBinding? = null
     private val binding get() = _binding!!
+    lateinit var preference: MyPreference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +30,16 @@ class OrderConfirmationFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentOrderConfirmationBinding.inflate(inflater, container, false)
+        preference = MyPreference.getInstance(requireContext())!!
 
 //        _binding!!.toAddressArrow.setOnClickListener {
 //            findNavController(this)?.navigate(R.id.address_fragment)
 //        }
+
+        _binding!!.nameTextView.text = preference.getData(Constants.USERFIRSTNAME)
+        _binding!!.emailTextView.text = preference.getData(Constants.USEREMAIL)
+       // _binding!!.phoneTextView.text = preference.getData(Constants.USERFIRSTNAME)
+
 
         val root: View = binding.root
         return root
