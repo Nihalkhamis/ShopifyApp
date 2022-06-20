da4f750ee6c6207333e95c155882b14c2b2f6a54
Rana-Abdelaziz <61909090+Rana-Abdelaziz@users.noreply.github.com>
2022-06-20 17:18:32 +0200

    Payment_Stripe
    Co-Authored-By: Hala Emad <35690579+HalaEH@users.noreply.github.com>

--- app/src/main/java/com/gradle/shopifyapp/payment/view/OrderConfirmationFragment.kt
+++ app/src/main/java/com/gradle/shopifyapp/payment/view/OrderConfirmationFragment.kt
@@ -8,11 +8,17 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.snackbar.Snackbar
 import com.gradle.shopifyapp.MainTabsActivity
 import com.gradle.shopifyapp.R
@@ -33,9 +39,13 @@ import com.paypal.android.sdk.payments.*
 import com.paypal.android.sdk.payments.PayPalService
 import com.paypal.android.sdk.payments.PaymentActivity
 import com.paypal.android.sdk.payments.PaymentConfirmation
+import com.stripe.android.PaymentConfiguration
+import com.stripe.android.paymentsheet.PaymentSheet
+import com.stripe.android.paymentsheet.PaymentSheetResult
 import org.json.JSONException
 import org.json.JSONObject
 import java.math.BigDecimal
+import java.util.HashMap
 
 class OrderConfirmationFragment : Fragment() {
     private var _binding: FragmentOrderConfirmationBinding? = null
@@ -61,12 +71,48 @@ class OrderConfirmationFragment : Fragment() {
     var draftOrderIds = ArrayList<Long>()
 
 
+    lateinit var btnPay : Button
 
+    val PUBLISH_KEY = "pk_test_51LC0btHd71OxeOPItO4IjE4Kezw2xz7wwAUCimJupenn5GmqczjnKviFl5nPUv8b9GcFJHdc3rTA57vo8jTC8Ti500Cpyp5Kng"
+    val SECRET_KEY = "sk_test_51LC0btHd71OxeOPI32npev8ME2kKBe1GqwdOL3fFiprgQU0OrMEqbhSTH2RYCn57E0laaKUZKtME096WGGHOK2jw00ey8OaAR4"
+    lateinit var paymentSheet : PaymentSheet
+    lateinit var customerId: String
+    lateinit var ephericalKey: String
+    lateinit var clientSecret: String
 
     var myAddress :Addresse = Addresse()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        PaymentConfiguration.init(requireContext(),PUBLISH_KEY)
+        paymentSheet = PaymentSheet(this, ::onPaymentSheetResult)
+
+        val request: StringRequest = object : StringRequest(Request.Method.POST, "https://api.stripe.com/v1/customers",
+            Response.Listener {response ->
+                try{
+                    val jsonObject = JSONObject(response)
+                    customerId = jsonObject.getString("id")
+                    Toast.makeText(requireContext(),"Customer Id: "+customerId,Toast.LENGTH_SHORT).show()
+                    getEphericalKey(customerId)
+                }catch (e : JSONException){
+                    e.printStackTrace()
+                }
+            },
+            Response.ErrorListener {
+                //
+            }) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String,String> {
+                val header: HashMap<String,String> = HashMap<String,String>()
+                header.put("Authorization" , "Bearer $SECRET_KEY")
+                return header
+            }
+        }
+
+        val requestQueue = Volley.newRequestQueue(requireContext())
+        requestQueue.add(request)
+
     }
 
     override fun onCreateView(
@@ -146,7 +192,7 @@ class OrderConfirmationFragment : Fragment() {
             var result_order_Model = makeOrderModel()
 
             if(_binding!!.creditRadioBtn.isChecked )
-                getPayment()
+                paymentFlow()
             if(!_binding!!.creditRadioBtn.isChecked && !_binding!!.cashRadioBtn.isChecked)
                 Toast.makeText(requireContext(),"Choose a payment method!",Toast.LENGTH_LONG).show()
             if(_binding!!.cashRadioBtn.isChecked)
@@ -202,85 +248,6 @@ class OrderConfirmationFragment : Fragment() {
         return Navigation.findNavController(view!!)
     }
 
-    private fun getPayment() {
-
-        // Creating a paypal payment on below line.
-        val payment = PayPalPayment(
-            BigDecimal(amount), "EUR", "Order Fees",
-            PayPalPayment.PAYMENT_INTENT_SALE
-        )
-
-        // Creating Paypal Payment activity intent
-        val intent = Intent(activity, PaymentActivity::class.java)
-
-        //putting the paypal configuration to the intent
-        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config)
-
-        // Putting paypal payment to the intent
-        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment)
-
-        // Starting the intent activity for result
-        // the request code will be used on the method onActivityResult
-        startActivityForResult(intent, PAYPAL_REQUEST_CODE)
-    }
-
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    Log.i("ACTIVITYRESSULT:","")
-        super.onActivityResult(requestCode, resultCode, data)
-        // If the result is from paypal
-        if (requestCode == PAYPAL_REQUEST_CODE) {
-
-            // If the result is OK i.e. user has not canceled the payment
-            if (resultCode == Activity.RESULT_OK) {
-
-                // Getting the payment confirmation
-                var confirm: PaymentConfirmation =
-                    data!!.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION)!!;
-
-                // if confirmation is not null
-                if (confirm != null) {
-                    try {
-                        // Getting the payment details
-                        var paymentDetails: String = confirm.toJSONObject().toString(4);
-                        // on below line we are extracting json response and displaying it in a text view.
-                        var payObj: JSONObject = JSONObject(paymentDetails);
-                        var payID: String = payObj.getJSONObject("response").getString("id");
-                        var state: String = payObj.getJSONObject("response").getString("state");
-                        Log.i("PAYMENT","Payment " + state + "\n with payment id is " + payID)
-                    } catch (e: JSONException) {
-                        // handling json exception on below line
-                        Log.e("Error", "an extremely unlikely failure occurred: ", e);
-                        Log.i("PAYMENT","Payment error")
-
-                    }
-                }
-            } else if (resultCode == Activity.RESULT_CANCELED) {
-                // on below line we are checking the payment status.
-                Log.i("paymentExample", "The user canceled.");
-            } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
-                // on below line when the invalid paypal config is submitted.
-                Log.i(
-                    "paymentExample",
-                    "An invalid Payment or PayPalConfiguration was submitted. Please see the docs."
-                );
-            }
-        }
-    }
-
-    companion object {
-        const val clientKey = "AVMXmO57ZhbNwUrxPpAEi7gBR6ftmNGCvDyzEXyICdGk0FPQrN9UbBuDZN--NLj78v6oBmQOAgHZ461K"
-        const val PAYPAL_REQUEST_CODE = 123
-
-        private val config = PayPalConfiguration() // Start with mock environment.  When ready,
-            // switch to sandbox (ENVIRONMENT_SANDBOX)
-            // or live (ENVIRONMENT_PRODUCTION)
-            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX) // on below line we are passing a client id.
-            .clientId(clientKey)
-    }
-
-
     private fun showSnackBar(msg:String){
         val snackBar = Snackbar.make(requireActivity().findViewById(android.R.id.content),
             msg, Snackbar.LENGTH_LONG
@@ -293,6 +260,89 @@ class OrderConfirmationFragment : Fragment() {
 //        Log.i("inside communicator method",myAddress.toString())
 //
 //    }
+
+    fun onPaymentSheetResult(paymentSheetResult: PaymentSheetResult) {
+        if (paymentSheetResult is PaymentSheetResult.Completed) {
+            Toast.makeText(requireContext(), "Payment Success!!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getEphericalKey(id: String){
+        val request: StringRequest = object : StringRequest(
+            Request.Method.POST, "https://api.stripe.com/v1/ephemeral_keys",
+            Response.Listener { response ->
+                try{
+                    val jsonObject = JSONObject(response)
+                    ephericalKey = jsonObject.getString("id")
+                    Toast.makeText(requireContext(),"Epherical Key: "+ephericalKey,Toast.LENGTH_SHORT).show()
+                    getClientSecret(customerId,ephericalKey)
+                }catch (e : JSONException){
+                    e.printStackTrace()
+                }
+            },
+            Response.ErrorListener {
+                //
+            }) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val header: HashMap<String, String> = HashMap<String,String>()
+                header.put("Authorization" , "Bearer $SECRET_KEY")
+                header.put("Stripe-Version" , "2020-08-27")
+                return header
+            }
+
+            override fun getParams(): Map<String, String> {
+                val param: HashMap<String, String> = HashMap<String,String>()
+                param.put("customer" ,customerId)
+                return param
+            }
+        }
+
+        val requestQueue = Volley.newRequestQueue(requireContext())
+        requestQueue.add(request)
+    }
+
+    private fun getClientSecret(customerId: String, ephericalKey: String) {
+        val request: StringRequest = object : StringRequest(Request.Method.POST, "https://api.stripe.com/v1/payment_intents",
+            Response.Listener {response ->
+                try{
+                    val jsonObject = JSONObject(response)
+                    clientSecret = jsonObject.getString("client_secret")
+                    Toast.makeText(requireContext(),"Client Secret: "+ clientSecret,Toast.LENGTH_SHORT).show()
+                    /// Lauch payment Flow
+                    // paymentFlow()
+                }catch (e : JSONException){
+                    e.printStackTrace()
+                }
+            },
+            Response.ErrorListener {
+                //
+            }) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val header: HashMap<String,String> = HashMap<String,String>()
+                header.put("Authorization" , "Bearer $SECRET_KEY")
+                return header
+            }
+
+            override fun getParams(): Map<String, String> {
+                val param: HashMap<String,String> = HashMap<String,String>()
+                param.put("customer" ,customerId)
+                param.put("amount" ,"10"+"00")
+                param.put("currency" ,"usd")
+                param.put("automatic_payment_methods[enabled]" ,"true")
+                return param
+            }
+        }
+
+        val requestQueue = Volley.newRequestQueue(requireContext())
+        requestQueue.add(request)
+    }
+
+    private fun paymentFlow() {
+        paymentSheet.presentWithPaymentIntent(clientSecret, PaymentSheet.Configuration("ITI",
+            PaymentSheet.CustomerConfiguration(customerId,ephericalKey)))
+    }
 
 
 }
